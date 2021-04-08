e09771d1337c43f6333094909507197f4c48ef02
vebits <vebjorni@me.com>
2021-04-08 20:19:58 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+  useLocation,
+} from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+
+import Home from "Home";
+import NotFound from "NotFound";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'EB Garamond', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  #root {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  
+  input {
+    font-family: 'EB Garamond', sans-serif;
+  }
+
+  a:hover, a:visited, a:link, a:active {
+    text-decoration: none;
+  }
+`;
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/404" component={NotFound} />
+          <Redirect to="/404" />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Page = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 104px 64px;
+`;
+
+const Title = styled.h2`
+  font-size: 3.5rem;
+  color: #3c3c3c;
+  margin-bottom: 24px;
+`;
+
+const Home = () => {
+  return (
+    <Page>
+      <Title noMargin>under construction...</Title>
+    </Page>
+  );
+};
+
+export default Home;
